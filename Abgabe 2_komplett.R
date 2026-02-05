ef2fdac89e91a2bd47aba0b74635312b6686f333
10Paddy05 <patrick.koller@uni-bielefeld.de>
2026-02-05 13:51:08 +0100

    Update 1 Abgabe 2_komplett.R

--- Abgabe 2_komplett.R
+++ Abgabe 2_komplett.R
@@ -1,10 +1,11 @@
-# Aufgabe 3.1 -----------------------------------
+# AUFGABE 3 -------------------------------------
+# ------ Aufgabe 3.1 ------
 
-  siegrunden <- 0      # Zaehle die gewonnenen Runden             
+  siegrunden <- 0     # Zaehle die gewonnenen Runden             
   iterations <- 1000  # Wie oft das Spiel gespielt werden soll
   
   for (i in 1:iterations){
-    offene_klappen <- c(1:12)  # Vektor mit den Klappen, die noch umgestossen werden müssen
+    offene_klappen <- c(1:12)  # Vektor mit den Klappen, die noch umgeklappt werden müssen
     weitermachen <- TRUE       # Soll die eine Runde fortgesetzt oder abgebrochen werden?
   
    
@@ -15,7 +16,7 @@
     augenzahl2<- sample(1:6, 1)
     wurf <- augenzahl1 + augenzahl2 # Zufaelliges Wurfergebnis
     wurf  
-    # 1. Möglichkeit: Falls der Wurf im Vektor enthalten ist, nimm diese Zahl aus den offenen Klappen heraus
+  # 1. Möglichkeit: Falls der Wurf direkt im Vektor enthalten ist, nimm diese Zahl aus den offenen Klappen heraus
         if (wurf %in% offene_klappen){ 
          offene_klappen <- offene_klappen[offene_klappen != wurf]
          weitermachen <- TRUE # Setze die aktuelle Runde oben fort
@@ -36,13 +37,13 @@
              }
   # 3. Möglichkeit: Es wird kein Paar gefunden             
           if (!gefunden) { 
-              weitermachen <- FALSE # Aktuelle Runde wird beendet und man bekommt ein Verloren zurück
+              weitermachen <- FALSE # Aktuelle Runde wird abgebrochen und man hat die Runde verloren
              
               break
               
             }
            }
-  
+  # 4. Möglichkeit: Der Vektor der zu umstoßenden Klappen ist leer
         if (length(offene_klappen) == 0) { # Falls der Vektor leer ist (keine Zahlen mehr zum umklappen) hat man gewonnen
           siegrunden <- siegrunden + 1     # erhoehe den Siegrundencounter um 1
           weitermachen <- FALSE
@@ -56,7 +57,7 @@
 siegrunden / iterations # Wahrscheinlichkeit, das Spiel zu gewinnen
 
 
-# Aufgabe 3.2 -----------------------------------
+# ------ Aufgabe 3.2 ------
 
 palindrom <- function(s) {
   Wort <- strsplit(s, "")[[1]]
@@ -77,7 +78,7 @@ test_that("palindrom funktioniert korrekt", {
 })
 
 
-# Aufgabe 3.3 ----------------------------------
+#  ------Aufgabe 3.3 ------
 # Teil 1
 data <- read.csv(
   file = "bike_sharing_data_(with_NAs)(6).csv",
@@ -90,16 +91,18 @@ class(data)
 
 # Teil 2
 data_60 <- subset(data, group == "60") 
-data_60
+data_60 # Wir geben die Daten zur Kontrolle aus, nur Gruppe 60
 
 # Teil 3
-anyNA(data_60)
-ids <- which(!complete.cases(data_60))
-data_60[ids,]
+anyNA(data_60) # Liegen NAs vor?
+ids <- which(!complete.cases(data_60)) 
+data_60[ids,] # Gib mir alle Zeilen, in denen NAs vorliegen
 
-ids <- which(is.na(data_60$precipitation))
-mean_precipitation <- mean(data_60$precipitation, na.rm = TRUE)
-data_60[ids, "precipitation"] <- mean_precipitation
+ids <- which(is.na(data_60$precipitation)) # Alle NAs, die in der Kategorie precipitation sind
+mean_precipitation <- mean(data_60$precipitation, na.rm = TRUE) # Mittelwert berechnen
+data_60[ids, "precipitation"] <- mean_precipitation # Datensatz überschreiben (NA durch Durchschnitt ersetzen)
+
+# Das Vorgehen wird für alle Attribute, in denen NAs vorliegen, gemacht
 
 ids <- which(is.na(data_60$windspeed))
 mean_windspeed <- mean(data_60$windspeed, na.rm = TRUE)
@@ -121,14 +124,14 @@ ids <- which(is.na(data_60$count))
 median_count <- median(data_60$count, na.rm = TRUE)
 data_60[ids, "count"] <- median_count
 
+anyNA(data_60) # Überprüfung: Liegen NAs vor? -> jetzt nicht mehr
+
 # Teil 4
 summary(data_60)
 
-         #nix unplausibles meiner meinung nach
-
-
+        
 #Teil 5
-count_jan <- subset(data_60, month_of_year == "1")$count
+count_jan <- subset(data_60, month_of_year == "1")$count #abspeichern eines Vektors mit den counts aus Januar
 count_feb <- subset(data_60, month_of_year == "2")$count
 count_mar <- subset(data_60, month_of_year == "3")$count
 count_apr <- subset(data_60, month_of_year == "4")$count
@@ -142,147 +145,6 @@ count_nov <- subset(data_60, month_of_year == "11")$count
 count_dec <- subset(data_60, month_of_year == "12")$count
 counts <- c(sum(count_jan), sum(count_feb),sum(count_mar),sum(count_apr),sum(count_may),
             sum(count_jun), sum(count_jul), sum(count_aug), sum(count_sep),
-            sum(count_oct), sum(count_nov), sum(count_dec))
+            sum(count_oct), sum(count_nov), sum(count_dec)) # Vektor, der die Summe aller ausgeliehenen Fahrrädern enthält.
 
-which.max(counts)
-
-# Aufgabe 4.1 ----------------------------
-#siehe Latex
-
-#Aufgabe 4.2 ------------------------
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("plotly")
-library(ggplot2)
-library(dplyr)
-library(plotly)
-
-ggplot(data = data_60) +
-  geom_point(aes(x = (average_temperature - 32) * 5/9,
-                 y = count)) +
-  xlab("Mittlere Temperatur (in °C)") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Anzahl ausgeliehener Fahrräder im Zusammenhang mit der Temperatur") +
-  theme_classic()
-
-ggplot(data =  data_60) +
-  geom_point(aes(x = precipitation, y = count)) +
-  xlab("Niederschlagsmenge (in Zoll)") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Ausgeliehene Fahrräder im Zusammenhang mit dem Niederschlag") +
-  theme_classic()
-
-ggplot(data =  data_60) +
-  geom_point(aes(x = windspeed *1.61, y = count)) +
-  xlab("Windgeschwindigkeit (in Kilometer pro Stunde)") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Ausgeliehene Fahrräder im Zusammenhang mit der Windgeschwindigkeit") +
-  theme_classic()
-
-ggplot(data =  data_60) +
-  geom_line(aes(x = day_of_year, y = count)) +
-  xlab("Tag des Jahres") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Ausgeliehene Fahrräder im Zusammenhang mit dem Tag des Jahres (2023)") +
-  theme_classic()
-
-# Aufgabe 4.3 -------------------------------
-
-data_60_rain <- data_60 %>%
-  filter(precipitation > 0) %>%
-  select(average_temperature, count)
-
-data_60_no_rain <- data_60 %>%
-  filter(precipitation == 0) %>%
-  select(average_temperature, count)
-
-
-
-ggplot(data = data_60_rain) +
-  geom_point(aes(x = (average_temperature - 32) * 5/9,
-                 y = count)) +
-  xlab("Mittlere Temperatur (in °C)") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Anzahl ausgeliehene Fahrräder bei Regen")+
-  theme_classic()
-
-ggplot(data = data_60_no_rain) +
-  geom_point(aes(x = (average_temperature - 32) * 5/9,
-                 y = count)) +
-  xlab("Mittlere Temperatur (in °C)") +
-  ylab("Anzahl ausgeliehener Fahrräder") +
-  ggtitle("Anzahl ausgeliehene Fahrräder ohne Regen")+
-  theme_classic()
-
-# Aufgabe 4.4 ------------
-
-ggplot(data = data_60) +
-  geom_histogram(aes(x = count),col="black") +
-  xlab("Anzahl ausgeliehener Fahrräder") +
-  ylab("Absolute Häufigkeit") +
-  ggtitle("Verteilung der Anzahl ausgeliehener Fahrräder")
-
-ggplot(data = data_60) +
-  geom_histogram(aes(x = (average_temperature - 32) * 5/9),col="black") +
-  xlab("Mittlere Temperatur (in °C)") +
-  ylab("Absolute Häufigkeit") +
-  ggtitle("Verteilung der mittleren Temperatur")
-
-ggplot(data = data_60) +
-  geom_histogram(aes(x = precipitation),col="black") +
-  xlab("Niederschlagsmenge (in Zoll)") +
-  ylab("Absolute Häufigkeit") +
-  ggtitle("Verteilung der Niederschlagsmenge")
-
-ggplot(data = data_60) +
-  geom_histogram(aes(x = windspeed *1.61),col="black") +
-  xlab("Windgeschwindigkeit (in km/h)") +
-  ylab("Absolute Häufigkeit") +
-  ggtitle("Verteilung der Windgeschwindigkeit")
-
-# Aufgabe 4.5 -----------------
-winter <- data_60 %>%
-  filter( month_of_year %in% c(12, 1, 2)) %>%
-  select(count)
-
-spring <- data_60 %>%
-  filter(month_of_year %in% c(3:5)) %>%
-  select(count)
-
-summer <- data_60 %>%
-  filter(month_of_year %in% c(6:8)) %>%
-  select(count)
-
-autmn <- data_60 %>%
-  filter(month_of_year %in% c(9:11)) %>%
-  select(count)
-
-
-fig <- plot_ly() %>% 
-  add_histogram(data = winter, x = ~count, name = "Winter", opacity = 0.7, marker = list(color = "black"), xbins = list(size = 2)) %>%
-  add_histogram(data = spring, x = ~count, name = "Frühling", opacity = 0.7, marker = list(color = "grey")) %>%
-  add_histogram(data = summer, x = ~count, name = "Sommer", opacity = 0.7, marker = list(color = "blue")) %>%
-  add_histogram(data = autmn, x = ~count, name = "Herbst", opacity = 0.5, marker = list(color = "orange")) %>%
-  layout(barmode = "stack", xaxis = list(title = "Anzahl ausgeliehener Fahrräder"), yaxis = list(title= "ABsolute Häufigkeit"),
-         title = "Histogramm der Anzahl ausgeliehener Farbe - sortiert nach Jahreszeit")
-
-fig
-# Aufgabe 4.6 ---------------------
-
-plot_ly(
-  data = data_60,
-  x = ~(average_temperature - 32 )* 5/9,
-  y = ~windspeed*1.61,
-  z = ~count,
-  type = "scatter3d",
-  mode = "markers",
-  color = ~count
-) %>% 
-  layout(
-  scene = list(
-      xaxis = list(title = "Mittlere Temperatur (in °C)"),
-      yaxis = list(title = "Windgeschwindigkeit (in km/h)"),
-      zaxis = list(title = "Anzahl ausgeliehener Fahrräder")
-    )
-  )
-
+which.max(counts) # Abfrage des Maximums
